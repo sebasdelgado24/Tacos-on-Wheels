6e06a07cfdfcb3b639cec3ac9eda6ceb5ece83b2
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-06-05 15:02:00 -0500

    All files and folders created also initial changes on the schema for database

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+### Schema 
+CREATE DATABASE tacos_db;
+SWITCH TO OR USE the `tacos_db`.
+   * Create a `tacos` table with these fields:
+     * **id**: an auto incrementing int that serves as the primary key.
+     * **taco_name**: a string.
+     * **shell**: a string (soft/hard ~ flour/corn)
+     * **vegetarian**: a boolean
+     * **picked_up**: a boolean.
