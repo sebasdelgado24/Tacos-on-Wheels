8a2a96aba5062282b6e1c62e215ef696ef5d6999
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-06-05 19:50:51 -0500

    seeds file created

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO tacos (taco_name) VALUES ('Shrimp Taco');
+INSERT INTO tacos (taco_name) VALUES ('Trompo Taco');
+INSERT INTO tacos (taco_name, vegetarian) VALUES ('Tofu Taco', true);
+INSERT INTO tacos (taco_name, picked_up) VALUES ('Fajita Taco', true);
+INSERT INTO tacos (taco_name, shell) VALUES ('soft flour');
+
+
+
