111a632e4a2b18fb9f82477d2929405486909b67
Juan Sebastian Delgado Rincon <juansebastiandelgadorincon@Juans-MacBook-Pro.local>
2019-06-06 18:16:52 -0500

    Heroku Deployment

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Manet1234",
-  database: "tacos_db"
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Manet1234",
+    database: "tacos_db"
+  });
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
